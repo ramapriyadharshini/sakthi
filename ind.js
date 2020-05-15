93e495be4f05cf392d419aec0f6702d251d16a78
ramapriyadharshini <ramamaha827@gmail.com>
2020-05-14 21:38:44 -0700

    my all files

---
+++ ind.js
@@ -0,0 +1,15 @@
+var element1 = document.getElementById("first")
+var element2 = document.getElementById("second")
+
+
+
+function displaydiv1 (){
+   element1.style.display="block"
+   element2.style.display="none"
+   
+}
+function displaydiv2 () {
+   element1.style.display="none"
+   element2.style.display="block"
+   
+}
